bea9f982bc96c94670a8bfb2ce06c924de9dc8d3
Zuha-Amjad3 <sp25-bcs-145@cuilahore.edu.pk>
2025-09-10 16:00:29 +0500

    Add files via upload

--- studentTest.java
+++ studentTest.java
@@ -6,9 +6,17 @@ public class studentTest{
 	public static void main(String args[]){
 		Student s1 = new Student();
 		
+		s1.setInfo("Ali", "ali@gmail.com", 3.2);
+
 		s1.displayInfo();
+		Student s2 = new Student("Zuha", "zuha@gmail.com", 4.0);
+
+		s2.displayInfo();
+
+		Student s3 = new Student("Fizza", "fizza@gmail.com", 2.6);
+		
+		System.out.println(s3.getName());
 
 	}
-
 
 }
