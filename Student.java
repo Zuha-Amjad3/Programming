c5ad0c3cccd6df04c0401bdff8201c58e16c63cb
Zuha-Amjad3 <sp25-bcs-145@cuilahore.edu.pk>
2025-09-10 15:55:51 +0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,20 @@
+//Zuha Amjad 
+//SP25-BCS-145
+
+public class Student{
+
+	private String name;
+	private String email;
+	private double cGPA;
+
+	public void displayInfo(){
+		System.out.println("  Student Details  ");
+		System.out.println("-------------------");
+		System.out.println("Name:   " + this.name);
+		System.out.println("Email:  " + this.email);
+		System.out.println("CGPA:   " + this.cGPA);
+
+	}
+
+
+}
---
+++ studentTest.java
@@ -0,0 +1,14 @@
+//Zuha Amjad
+//SP25-BCS-145
+
+public class studentTest{
+
+	public static void main(String args[]){
+		Student s1 = new Student();
+		
+		s1.displayInfo();
+
+	}
+
+
+}
